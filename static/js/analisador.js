6579ad2b83aefb7ebc418b7c7af6fe250973a34b
giovane.correa <giovane.correa@4upsites.com.br>
2019-06-15 14:13:52 -0300

    refactor

--- static/js/analisador.js
+++ static/js/analisador.js
@@ -38,7 +38,7 @@ var tBody = "";
 var win = 0;
 var doc = 0;
 var concluido = false;
-$(document).ready(function(){
+$(document).ready(function () {
     win = $(window).height();
     doc = $(document).height();
 });
@@ -62,7 +62,6 @@ function gerarPilha() {
     } else {
         while (cabecote != "$$" && error == false) {
             executar();
-            console.log(doc);
             scrollToFooter();
         }
     }
@@ -70,35 +69,60 @@ function gerarPilha() {
 
 }
 
-function nextStep(){
+function nextStep() {
     executar();
     scrollToFooter();
     doc = $(document).height();
-    if(doc > win){
+    if (doc > win) {
         $(".btn-next-footer").removeClass("css-none");
     }
 }
 
-function executar(){
-    if(!concluido){
-    iteracoes++;
-            var cabecaCabecote = cabecote.substring(0, 1);
+function executar() {
+    if (!concluido) {
+        iteracoes++;
+        var cabecaCabecote = cabecote.substring(0, 1);
 
-            var acao = verificaPilhaComEntrada(pilha.substring(pilha.length - 1, pilha.length), cabecaCabecote);
-            
-            if (acao == 'S' || acao == 'A' || acao == 'B' || acao == 'C') {
-                var pilhaAcao = acoes[acao][cabecaCabecote];
+        var acao = verificaPilhaComEntrada(pilha.substring(pilha.length - 1, pilha.length), cabecaCabecote);
 
-            }
-            if (pilha == '$' && cabecaCabecote == '$') {
-                html += "<tr>" +
-                    "<th scope='col'>" + pilha + "</th>" +
-                    "<th scope='col'>" + cabecote + "</th>" +
-                    "<th scope='col'>Aceito em " + iteracoes + " iteracoes </th>" +
-                    "</tr>";
-                cabecote = "$$";
-                concluido = true;
-            } else if (acao == "erro") {
+        if (acao == 'S' || acao == 'A' || acao == 'B' || acao == 'C') {
+            var pilhaAcao = acoes[acao][cabecaCabecote];
+
+        }
+        if (pilha == '$' && cabecaCabecote == '$') {
+            html += "<tr>" +
+                "<th scope='col'>" + pilha + "</th>" +
+                "<th scope='col'>" + cabecote + "</th>" +
+                "<th scope='col'>Aceito em " + iteracoes + " iteracoes </th>" +
+                "</tr>";
+            cabecote = "$$";
+            concluido = true;
+        } else if (acao == "erro") {
+            html += "<tr>" +
+                "<th scope='col'>" + pilha + "</th>" +
+                "<th scope='col'>" + cabecote + "</th>" +
+                "<th scope='col'>Erro em " + iteracoes + " iterações</th>" +
+                "</tr>";
+            error = true;
+            concluido = true;
+        } else if (acao == "ler") {
+            html += "<tr>" +
+                "<th scope='col'>" + pilha + "</th>" +
+                "<th scope='col'>" + cabecote + "</th>" +
+                "<th scope='col'>Ler " + cabecaCabecote + "</th>" +
+                "</tr>";
+            cabecote = cabecote.substring(1, cabecote.length);
+            pilha = pilha.substring(0, pilha.length - 1);
+        } else if (pilhaAcao == "ep") {
+            html += "<tr>" +
+                "<th scope='col'>" + pilha + "</th>" +
+                "<th scope='col'>" + cabecote + "</th>" +
+                "<th scope='col'>" + acao + " -> ε</th>" +
+                "</tr>";
+            pilha = pilha.substring(0, pilha.length - 1);
+
+        } else {
+            if (pilhaAcao == "-") {
                 html += "<tr>" +
                     "<th scope='col'>" + pilha + "</th>" +
                     "<th scope='col'>" + cabecote + "</th>" +
@@ -106,46 +130,21 @@ function executar(){
                     "</tr>";
                 error = true;
                 concluido = true;
-            } else if (acao == "ler") {
-                html += "<tr>" +
-                    "<th scope='col'>" + pilha + "</th>" +
-                    "<th scope='col'>" + cabecote + "</th>" +
-                    "<th scope='col'>Ler " + cabecaCabecote + "</th>" +
-                    "</tr>";
-                cabecote = cabecote.substring(1, cabecote.length);
-                pilha = pilha.substring(0, pilha.length - 1);
-            } else if (pilhaAcao == "ep") {
-                html += "<tr>" +
-                    "<th scope='col'>" + pilha + "</th>" +
-                    "<th scope='col'>" + cabecote + "</th>" +
-                    "<th scope='col'>" + acao + " -> ε</th>" +
-                    "</tr>";
-                pilha = pilha.substring(0, pilha.length - 1);
-
             } else {
-                if (pilhaAcao == "-") {
-                    html += "<tr>" +
-                        "<th scope='col'>" + pilha + "</th>" +
-                        "<th scope='col'>" + cabecote + "</th>" +
-                        "<th scope='col'>Erro em " + iteracoes + " iterações</th>" +
-                        "</tr>";
-                    error = true;
-                    concluido = true;
-                } else {
-                    html += "<tr>" +
-                        "<th scope='col'>" + pilha + "</th>" +
-                        "<th scope='col'>" + cabecote + "</th>" +
-                        "<th scope='col'>" + acao + " -> " + pilhaAcao + "</th>" +
-                        "</tr>";
-                    pilha = pilha.substring(0, pilha.length - 1);
-                    pilha += pilhaAcao.split('').reverse().join('');
-
-                }
+                html += "<tr>" +
+                    "<th scope='col'>" + pilha + "</th>" +
+                    "<th scope='col'>" + cabecote + "</th>" +
+                    "<th scope='col'>" + acao + " -> " + pilhaAcao + "</th>" +
+                    "</tr>";
+                pilha = pilha.substring(0, pilha.length - 1);
+                pilha += pilhaAcao.split('').reverse().join('');
 
             }
 
-            tBody.html(html);
         }
+
+        tBody.html(html);
+    }
 }
 
 function verificaPilhaComEntrada(pilha, cabecote) {
@@ -171,19 +170,22 @@ function verificaNaoTerminal(caracter) {
     return false;
 }
 
-function clean(){
+function clean() {
     $(".table-pilha tbody").html("");
     $(".table-pilha").addClass("css-none");
     $(".btn-next-footer").addClass("css-none");
     $(".table-first-follow").removeClass("css-none");
     $("#sentenca").val("");
-    $("#stepByStep").prop("checked",false);
-    $("html").animate({scrollTop:0},"slow");
+    $("#stepByStep").prop("checked", false);
+    $("html").animate({ scrollTop: 0 }, "slow");
     concluido = false;
 }
 
-function scrollToFooter(){
-    $("html").animate({scrollTop:doc},"slow");
+function scrollToFooter() {
+    if (!concluido) {
+
+        $("html").animate({ scrollTop: doc }, "slow");
+    }
 }
 
 function gerarSentenca() {
@@ -193,69 +195,68 @@ function gerarSentenca() {
     $(".row-S").addClass("row-to-select");
 }
 
-function addAcao(naoTerminal, terminal){
+function addAcao(naoTerminal, terminal) {
     $(".row-" + naoTerminal).removeClass("row-to-select");
     var input = pilhaGerarSentenca;
     var nextAction = acoes[naoTerminal][terminal];
-    console.log("next: " + nextAction);
-    if(nextAction != "ep"){
-    input = input.replace(naoTerminal, nextAction);
+    if (nextAction != "ep") {
+        input = input.replace(naoTerminal, nextAction);
 
-    for(var i = 0; i < nextAction.length; i++){
-        var naoTerminalFlag = verificaNaoTerminal(nextAction[i]);
-        if(naoTerminalFlag){
-            $(".row-" + nextAction[i]).addClass("row-to-select");
-            break;
-        }
-    }
-    pilhaGerarSentenca = input;
-    $("#sentenca-gerar").val(input);
-}else{
-    input = "";
-    var countNaoTerminal = 0;
-    var hasNaoTerminal = false;
-    for(var i = 0; i < pilhaGerarSentenca.length; i++){
-        var naoTerminalFlag = verificaNaoTerminal(pilhaGerarSentenca[i]);
-        if(naoTerminalFlag && countNaoTerminal == 0){
-            $(".row-" + pilhaGerarSentenca[i]).removeClass("row-to-select");
-
-            
-        }else if(naoTerminal){
-            if(!hasNaoTerminal){
-                $(".row-" + pilhaGerarSentenca[i]).addClass("row-to-select");
-                hasNaoTerminal = true;
-                
+        for (var i = 0; i < nextAction.length; i++) {
+            var naoTerminalFlag = verificaNaoTerminal(nextAction[i]);
+            if (naoTerminalFlag) {
+                $(".row-" + nextAction[i]).addClass("row-to-select");
+                break;
             }
-            input += pilhaGerarSentenca[i];
-        }else{
-            input += pilhaGerarSentenca[i];
         }
+        pilhaGerarSentenca = input;
+        $("#sentenca-gerar").val(input);
+    } else {
+        input = "";
+        var countNaoTerminal = 0;
+        var hasNaoTerminal = false;
+        for (var i = 0; i < pilhaGerarSentenca.length; i++) {
+            var naoTerminalFlag = verificaNaoTerminal(pilhaGerarSentenca[i]);
+            if (naoTerminalFlag && countNaoTerminal == 0) {
+                $(".row-" + pilhaGerarSentenca[i]).removeClass("row-to-select");
+
+
+            } else if (naoTerminal) {
+                if (!hasNaoTerminal) {
+                    $(".row-" + pilhaGerarSentenca[i]).addClass("row-to-select");
+                    hasNaoTerminal = true;
+
+                }
+                input += pilhaGerarSentenca[i];
+            } else {
+                input += pilhaGerarSentenca[i];
+            }
+        }
+        if (!hasNaoTerminal) {
+            $(".row-" + naoTerminal).removeClass("row-to-select");
+        }
+        pilhaGerarSentenca = input;
+        $("#sentenca-gerar").val(input);
     }
-    if(!hasNaoTerminal){
-        $(".row-" + naoTerminal).removeClass("row-to-select");
-    }
-    pilhaGerarSentenca = input;
-    $("#sentenca-gerar").val(input);
-}
 
 }
 
-function closeModal(){
+function closeModal() {
     $(".modal-backdrop").addClass("fade");
     $(".modal-backdrop").removeClass("show");
     $("#modalGerarSentenca").hide();
     $("#sentenca-gerar").val("");
 }
 
-function copiarSentenca(){
-    
+function copiarSentenca() {
+
     var inputSentenca = $("#sentenca-gerar").val();
     $("#sentenca").val(inputSentenca);
     closeModal();
 }
 
-$(window).scroll(function() {
-    if($(this).scrollTop() <= $(window).height() && concluido){
+$(window).scroll(function () {
+    if ($(this).scrollTop() <= $(window).height() && concluido) {
         $(".btn-next-footer").addClass("css-none");
     }
 });
